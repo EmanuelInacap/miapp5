791512751267183a0bc0b7f8a0d206f09aab82d9
EmanuelInacap <emanuel.sanchez03@inacapmail.cl>
2024-06-28 15:34:55 -0400

    Updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
+import './App.css'
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+        <Form>
+          <fieldset disabled>
+            <Form.Group className="mb-3">
+              <Form.Label htmlFor="disabledTextInput">Disabled input</Form.Label>
+              <Form.Control id="disabledTextInput" placeholder="Disabled input" />
+            </Form.Group>
+            <Form.Group className="mb-3">
+              <Form.Label htmlFor="disabledSelect">Disabled select menu</Form.Label>
+              <Form.Select id="disabledSelect">
+                <option>Disabled select</option>
+              </Form.Select>
+            </Form.Group>
+            <Form.Group className="mb-3">
+              <Form.Check
+                type="checkbox"
+                id="disabledFieldsetCheck"
+                label="Can't check this"
+              />
+            </Form.Group>
+            <Button type="submit">Submit</Button>
+          </fieldset>
+        </Form>
   )
 }
 
